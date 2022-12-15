2e273f613f62c7e0f066ae312ec1874850d7e932
Krzysztof027 <krzysztofszczerkowski1492@gmail.com>
2022-12-15 15:00:15 +0100

    Listy cz1

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kart_Listy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Uczniowie> DodajUcznia { get; set; } //samo będzie się aktualizować
+        //Reprezentuje dynamiczną kolekcję danych, która dostarcza powiadomienia po dodaniu lub usunięciu elementów albo odświeżeniu całej listy.
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            zapiszUczniow();
+        }
+        private void zapiszUczniow()
+        {
+            DodajUcznia = new ObservableCollection<Uczniowie>();
+            DodajUcznia.Add(new Uczniowie("Zosia", "Samosia", false, "3P")); ;
+            DodajUcznia.Add(new Uczniowie("Edek", "Fedek", true, "3P"));
+        }
+    }
+}
---
+++ Uczniowie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace kart_Listy
+{
+    public partial class MainWindow : Window
+    {
+
+        public partial class Uczniowie
+        {
+
+            public String Imie { get; set; }
+            public String Nazwisko { get; set; }
+            public bool Plec { get; set; }
+            public String Klasa { get; set; }
+
+            public Uczniowie(string imie, string nazwisko, bool plec, string klasa)
+            {
+                Imie = imie;
+                Nazwisko = nazwisko;
+                //this.Plec = plec;
+                Plec = plec;
+                Klasa = klasa;
+            }
+        }
+
+    }
+}
